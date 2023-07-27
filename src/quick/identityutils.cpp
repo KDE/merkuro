1bb8e3e175f667f16631741f9db29af93b591a2a
Claudio Cambra <claudio.cambra@kde.org>
2023-07-27 18:17:47 +0800

    Add a starter identityutils class
    Signed-off-by: Claudio Cambra <claudio.cambra@kde.org>

---
+++ src/quick/identityutils.cpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Claudio Cambra <claudio.cambra@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#include "identityutils.h"
+#include <KLocalizedString>
+#include <qobject.h>
+
+namespace Akonadi
+{
+namespace Quick
+{
+}
+}
