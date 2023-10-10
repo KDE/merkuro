584bb1fa4072bf044e53ec1a9228ebbbe2efc512
Carl Schwan <carl@carlschwan.eu>
2023-10-09 20:09:41 -0400

    Remove dead code

--- src/calendar/qml/CalendarUtils/dateutils.js
+++ src/calendar/qml/CalendarUtils/dateutils.js
@@ -3,37 +3,6 @@
 // SPDX-FileCopyrightText: 2021 Claudio Cambra <claudio.cambra@gmail.com>
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-/**
-* Returns the week number for this date.  dowOffset is the day of week the week
-* "starts" on for your locale - it can be from 0 to 6. If dowOffset is 1 (Monday),
-* the week returned is the ISO 8601 week number.
-* @param int dowOffset
-* @return int
-*/
-function getWeek(date, dowOffset) {
-    let newYear = new Date(date.getFullYear(),0,1);
-    let day = newYear.getDay() - dowOffset; //the day of week the year begins on
-    day = (day >= 0 ? day : day + 7);
-    let daynum = Math.floor((date.getTime() - newYear.getTime() - (date.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
-    let weeknum;
-    //if the year starts before the middle of a week
-    if(day < 4) {
-        weeknum = Math.floor((daynum+day-1)/7) + 1;
-        if(weeknum > 52) {
-            let nYear = new Date(date.getFullYear() + 1,0,1);
-            let nday = nYear.getDay() - dowOffset;
-            nday = nday >= 0 ? nday : nday + 7;
-            /*if the next year starts before the middle of
-            the week, it is week #1 of that year*/
-            weeknum = nday < 4 ? 1 : 53;
-        }
-    }
-    else {
-        weeknum = Math.floor((daynum+day-1)/7);
-    }
-    return weeknum;
-}
-
 function roundToDay(date) {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate())
 }
