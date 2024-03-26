4aef5d166c218b36a71b220bfdcc54f5c2973158
Laurent Montel <montel@kde.org>
2024-03-26 08:09:29 +0100

    Fix translated shortcut
    CCBUG: 484281

--- src/lib/actionsmodel.cpp
+++ src/lib/actionsmodel.cpp
@@ -107,7 +107,7 @@ QVariant KalCommandBarModel::data(const QModelIndex &index, int role) const
             QString actionText = KLocalizedString::removeAcceleratorMarker(entry.action->text());
             return QString(groupName + QStringLiteral(": ") + actionText);
         } else {
-            return entry.action->shortcut().toString();
+            return entry.action->shortcut().toString(QKeySequence::NativeText);
         }
     case ShortcutRole:
         return entry.action->shortcut().toString();
