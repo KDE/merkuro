e07e5e273abbf072c1cebc33eda1fbe9379dee68
Laurent Montel <montel@kde.org>
2024-06-11 08:31:10 +0200

    Use uri directly here too

--- src/contacts/contactplugin.cpp
+++ src/contacts/contactplugin.cpp
@@ -19,31 +19,31 @@ void CalendarPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QByteArray("org.kde.merkuro.contact"));
 
-    qmlRegisterSingletonType<ContactApplication>("org.kde.merkuro.contact", 1, 0, "ContactApplication", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
+    qmlRegisterSingletonType<ContactApplication>(uri, 1, 0, "ContactApplication", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
         Q_UNUSED(engine)
         Q_UNUSED(scriptEngine)
         return new ContactApplication;
     });
 
-    qmlRegisterSingletonType<ContactConfig>("org.kde.merkuro.contact", 1, 0, "Config", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
+    qmlRegisterSingletonType<ContactConfig>(uri, 1, 0, "Config", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
         Q_UNUSED(engine)
         Q_UNUSED(scriptEngine)
         return new ContactConfig;
     });
 
-    qmlRegisterSingletonType<ContactManager>("org.kde.merkuro.contact", 1, 0, "ContactManager", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
+    qmlRegisterSingletonType<ContactManager>(uri, 1, 0, "ContactManager", [](QQmlEngine *engine, QJSEngine *scriptEngine) {
         Q_UNUSED(engine)
         Q_UNUSED(scriptEngine)
         return new ContactManager;
     });
 
-    qmlRegisterUncreatableType<EmailModel>("org.kde.merkuro.contact", 1, 0, "EmailModel", QStringLiteral("Enum"));
-    qmlRegisterUncreatableType<PhoneModel>("org.kde.merkuro.contact", 1, 0, "PhoneModel", QStringLiteral("Enum"));
-    qmlRegisterType<AddresseeWrapper>("org.kde.merkuro.contact", 1, 0, "AddresseeWrapper");
-    qmlRegisterType<ContactEditorBackend>("org.kde.merkuro.contact", 1, 0, "ContactEditor");
-    qmlRegisterType<ContactGroupWrapper>("org.kde.merkuro.contact", 1, 0, "ContactGroupWrapper");
-    qmlRegisterType<ContactGroupEditor>("org.kde.merkuro.contact", 1, 0, "ContactGroupEditor");
-    qmlRegisterType<ContactsModel>("org.kde.merkuro.contact", 1, 0, "ContactsModel");
+    qmlRegisterUncreatableType<EmailModel>(uri, 1, 0, "EmailModel", QStringLiteral("Enum"));
+    qmlRegisterUncreatableType<PhoneModel>(uri, 1, 0, "PhoneModel", QStringLiteral("Enum"));
+    qmlRegisterType<AddresseeWrapper>(uri, 1, 0, "AddresseeWrapper");
+    qmlRegisterType<ContactEditorBackend>(uri, 1, 0, "ContactEditor");
+    qmlRegisterType<ContactGroupWrapper>(uri, 1, 0, "ContactGroupWrapper");
+    qmlRegisterType<ContactGroupEditor>(uri, 1, 0, "ContactGroupEditor");
+    qmlRegisterType<ContactsModel>(uri, 1, 0, "ContactsModel");
     qRegisterMetaType<KContacts::Picture>("KContacts::Picture");
     qRegisterMetaType<KContacts::PhoneNumber::List>("KContacts::PhoneNumber::List");
     qRegisterMetaType<KContacts::PhoneNumber>("KContacts::PhoneNumber");
