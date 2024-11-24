b7fc3eeefafd1684429aad44988cdb0c3102d5bd
Claudio Cambra <claudio.cambra@kde.org>
2024-11-24 18:42:55 +0800

    Remove qDebug from addDayDates

--- src/calendar/models/infinitemerkurocalendarviewmodel.cpp
+++ src/calendar/models/infinitemerkurocalendarviewmodel.cpp
@@ -228,7 +228,6 @@ void InfiniteMerkuroCalendarViewModel::addDayDates(const bool atEnd, const QDate
 
     for (int i = 0; i < m_datesToAdd; i++) {
         QDate startDate = startFrom.isValid() && i == 0 ? startFrom : atEnd ? m_startDates[rowCount() - 1].addDays(amount) : m_startDates[0].addDays(-amount);
-        qDebug() << "addDayDate" << startDate;
 
         if (atEnd) {
             m_startDates.append(startDate);
