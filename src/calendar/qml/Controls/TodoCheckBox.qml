KDE/merkuro

commit a8f18c08670f435034dc37b8a269aaf590257925

Port calendar to declarative type registration

src/calendar/qml/Controls/TodoCheckBox.qml
@@ -7,8 +7,6 @@ import QtQuick.Layouts
 import QtQuick.Controls as QQC2
 import org.kde.kirigami as Kirigami
 
-import org.kde.merkuro.utils
-
 ColoredCheckbox {
     id: todoCheckbox
 

commit 907786815306937e293b0b198ef1813f6ec7bee7

Fix not being able to check any tasks in the list
This was broken in a8f18c08670f435034dc37b8a269aaf590257925 because it
didn't re-import the new module it lives in (like in
MainDRawerToggleButton, for example.)

src/calendar/qml/Controls/TodoCheckBox.qml
@@ -6,6 +6,7 @@ import QtQuick
 import QtQuick.Layouts
 import QtQuick.Controls as QQC2
 import org.kde.kirigami as Kirigami
+import org.kde.merkuro.calendar as Calendar
 
 ColoredCheckbox {
     id: todoCheckbox
@@ -17,7 +18,7 @@ ColoredCheckbox {
     color: incidenceColor
     radius: 100
     checked: todoCompleted || todoCompletion === 100
-    onClicked: CalendarUiUtils.completeTodo(todoPtr)
+    onClicked: Calendar.CalendarUiUtils.completeTodo(todoPtr)
 
     indicator: Item {
         height: parent.height