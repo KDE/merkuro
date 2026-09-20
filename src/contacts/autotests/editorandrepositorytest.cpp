bf052ba85185203c50460dc8c8a00936fecb340d
Carl Schwan <carl@carlschwan.eu>
2026-09-20 21:18:04 +0200

    contacts: Add and fix tests

--- src/contacts/autotests/editorandrepositorytest.cpp
+++ src/contacts/autotests/editorandrepositorytest.cpp
@@ -3,10 +3,13 @@
 
 #include "../addresseewrapper.h"
 #include "../contacteditorbackend.h"
+#include "../contactgroupeditor.h"
 #include "../contactrepository.h"
 
 #include <Akonadi/CollectionFetchJob>
 #include <Akonadi/ItemFetchJob>
+#include <KContacts/Addressee>
+#include <KContacts/ContactGroup>
 #include <KJob>
 #include <QAbstractItemModel>
 #include <QSignalSpy>
@@ -26,11 +29,12 @@ private Q_SLOTS:
         AkonadiTest::checkTestIsIsolated();
 
         m_repository = std::make_unique<ContactRepository>();
-        QTRY_VERIFY_WITH_TIMEOUT(m_repository->contactCollections()->rowCount() > 0, 10000);
+        QVERIFY(m_repository->filteredContacts());
+        QTRY_VERIFY_WITH_TIMEOUT(m_repository->contactCollections()->rowCount() > 0, 5000);
 
         auto collectionJob = new Akonadi::CollectionFetchJob(Akonadi::Collection::root(), Akonadi::CollectionFetchJob::Recursive, this);
         QSignalSpy collectionResult(collectionJob, &KJob::result);
-        QVERIFY(collectionResult.wait(10000));
+        QVERIFY(collectionResult.wait(5000));
         QVERIFY2(!collectionJob->error(), qPrintable(collectionJob->errorString()));
 
         for (const auto &collection : collectionJob->collections()) {
@@ -47,17 +51,27 @@ private Q_SLOTS:
         auto itemJob = new Akonadi::ItemFetchJob(m_contactCollection, this);
         itemJob->fetchScope().fetchFullPayload();
         QSignalSpy itemResult(itemJob, &KJob::result);
-        QVERIFY(itemResult.wait(10000));
+        QVERIFY(itemResult.wait(3000));
         QVERIFY2(!itemJob->error(), qPrintable(itemJob->errorString()));
         QVERIFY(!itemJob->items().isEmpty());
+
+        for (const auto &item : itemJob->items()) {
+            if (item.mimeType() == KContacts::Addressee::mimeType()) {
+                m_contactItem = item;
+            } else if (item.mimeType() == KContacts::ContactGroup::mimeType()) {
+                m_contactGroupItem = item;
+            }
+        }
+        QVERIFY(m_contactItem.isValid());
+        QVERIFY(m_contactGroupItem.isValid());
 
         ContactEditorBackend editor;
         editor.setMode(ContactEditorBackend::EditMode);
         QSignalSpy contactChanged(&editor, &ContactEditorBackend::contactChanged);
         QSignalSpy errors(&editor, &ContactEditorBackend::errorOccured);
-        editor.setItem(itemJob->items().first());
+        editor.setItem(m_contactItem);
 
-        QVERIFY(contactChanged.wait(10000));
+        QVERIFY(contactChanged.wait(3000));
         QVERIFY(errors.isEmpty());
         QVERIFY(editor.contact());
         QCOMPARE(editor.contact()->formattedName(), u"Ada Lovelace"_s);
@@ -67,15 +81,81 @@ private Q_SLOTS:
         QSignalSpy finished(&editor, &ContactEditorBackend::finished);
         editor.contact()->setNote(u"Updated by the editor test"_s);
         editor.saveContactInAddressBook();
-        QTRY_VERIFY_WITH_TIMEOUT(finished.count() > 0, 10000);
+        QTRY_VERIFY_WITH_TIMEOUT(finished.count() > 0, 3000);
         QCOMPARE(savingChanged.count(), 2);
         QVERIFY(!editor.saving());
         QVERIFY(errors.isEmpty());
     }
 
+    void editorRejectsMissingAddressBook()
+    {
+        ContactEditorBackend editor;
+        QSignalSpy errors(&editor, &ContactEditorBackend::errorOccured);
+
+        editor.contact()->setFormattedName(u"A contact without an address book"_s);
+        editor.saveContactInAddressBook();
+
+        QCOMPARE(errors.count(), 1);
+        QCOMPARE(errors.first().first().toString(), u"No address book selected."_s);
+        QVERIFY(!editor.saving());
+    }
+
+    void groupEditorLoadsAndStoresGroup()
+    {
+        ContactGroupEditor editor;
+        editor.setMode(ContactGroupEditor::EditMode);
+        QSignalSpy nameChanged(&editor, &ContactGroupEditor::nameChanged);
+        QSignalSpy errors(&editor, &ContactGroupEditor::errorOccured);
+        editor.loadContactGroup(m_contactGroupItem);
+
+        QVERIFY(nameChanged.wait(3000));
+        QVERIFY(errors.isEmpty());
+        QCOMPARE(editor.name(), u"KDE Friends"_s);
+        QCOMPARE(editor.collectionId(), m_contactCollection.id());
+
+        QSignalSpy savingChanged(&editor, &ContactGroupEditor::savingChanged);
+        QSignalSpy finished(&editor, &ContactGroupEditor::finished);
+        editor.setName(u"Updated KDE Friends"_s);
+        QVERIFY(editor.saveContactGroup());
+        QTRY_VERIFY_WITH_TIMEOUT(finished.count() > 0, 3000);
+        QCOMPARE(savingChanged.count(), 2);
+        QVERIFY(!editor.saving());
+        QVERIFY(errors.isEmpty());
+    }
+
+    void editorsReportFetchErrors()
+    {
+        ContactEditorBackend contactEditor;
+        contactEditor.setMode(ContactEditorBackend::EditMode);
+        QSignalSpy contactErrors(&contactEditor, &ContactEditorBackend::errorOccured);
+        contactEditor.setItem(Akonadi::Item(999999));
+        QVERIFY(contactErrors.wait(3000));
+        QCOMPARE(contactErrors.count(), 1);
+
+        ContactGroupEditor groupEditor;
+        groupEditor.setMode(ContactGroupEditor::EditMode);
+        QSignalSpy groupErrors(&groupEditor, &ContactGroupEditor::errorOccured);
+        groupEditor.loadContactGroup(Akonadi::Item(999998));
+        QVERIFY(groupErrors.wait(3000));
+        QCOMPARE(groupErrors.count(), 1);
+    }
+
+    void groupEditorRejectsMissingAddressBook()
+    {
+        ContactGroupEditor editor;
+        QSignalSpy errors(&editor, &ContactGroupEditor::errorOccured);
+
+        QVERIFY(!editor.saveContactGroup());
+        QCOMPARE(errors.count(), 1);
+        QCOMPARE(errors.first().first().toString(), u"No address book selected"_s);
+        QVERIFY(!editor.saving());
+    }
+
 private:
     std::unique_ptr<ContactRepository> m_repository;
     Akonadi::Collection m_contactCollection;
+    Akonadi::Item m_contactItem;
+    Akonadi::Item m_contactGroupItem;
 };
 
 QTEST_MAIN(EditorAndRepositoryTest)
